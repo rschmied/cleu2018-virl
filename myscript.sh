0723ea7a7e8b69d1ca08842a8f40a4f85b682897
Ralph Schmieder <rschmied@cisco.com>
2017-05-31 19:40:28 +0200

    more stuff
    Signed-off-by: Ralph Schmieder <rschmied@cisco.com>

--- myscript.sh
+++ myscript.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+BUILD_HOST="rschmied@172.17.0.1"
+VIRL_HOST=""
+
 echo "building stuff..."
-ssh rschmied@172.17.0.1 'source venv/bin/activate; virltester --help'
+
+echo "copying files to destination"
+scp -r project/ ${BUILD_HOST}:
+ssh ${BUILD_HOST} '
+source venv/bin/activate;
+cd project
+touch GAGA
+virltester --help
+'
+
+echo "copying file back"
+scp -R ${BUILD_HOST}/project/ .
+
 exit 0
