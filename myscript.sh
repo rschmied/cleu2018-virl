777a9f3d5e3268d517184085620d5f3c774fe116
Ralph Schmieder <rschmied@cisco.com>
2017-05-31 22:11:51 +0200

    meh
    Signed-off-by: Ralph Schmieder <rschmied@cisco.com>

--- myscript.sh
+++ myscript.sh
@@ -1,38 +1,43 @@
 #!/bin/bash
 
 BUILD_HOST="rschmied@172.17.0.1"
-PROJECTS="projects"
 
 echo "building stuff..."
-#ls -la
-#env
-
 echo "copying files to destination"
-scp -r ${PROJECTS}/ ${BUILD_HOST}:
-ssh ${BUILD_HOST} "
+scp 2>&1 -r projects/ ${BUILD_HOST}:
+
+
+ssh ${BUILD_HOST} '
+'VIRL_HOST=$VIRL_HOST
+VIRL_PORT=$VIRL_PORT'
 
 # activate the environment
 source venv/bin/activate
-cd $PROJECTS
+cd projects
 
 # create the log directory and empty it
 test -d LOGS || mkdir LOGS
 rm LOGS/*
 
 # run the sim for all sim test definitions
-for v in $(find . -name '*.yml' -type f); do
-    virltester 2>&1 --nocolor $v | tee $(basename -s yml $v)log
+#for v in \"$(find . -name *.yml -type f)\"; do
+echo "HOST:"$VIRL_HOST
+for file in ./*.yml
+do
+    echo \"[\"$file\"]\"
+    virltester 2>&1 --nocolor $file | tee $(basename -s yml $file)log
 done
-
 # move all log files into the artifacts dir
 mv *.log LOGS
-"
+
+'
+
 
 retcode=$?
 
 if [ $retcode -eq 0 ]; then
     echo "copying file back"
-    scp -r ${BUILD_HOST}:${PROJECTS}/ .
+    scp -r ${BUILD_HOST}:projects/LOGS/ .
 fi
 
 exit $retcode
