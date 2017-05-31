955b55161cc068572a7d85fbbc66a5f81d35ea3d
Ralph Schmieder <rschmied@cisco.com>
2017-05-31 20:54:10 +0200

    aha
    Signed-off-by: Ralph Schmieder <rschmied@cisco.com>

--- myscript.sh
+++ myscript.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 BUILD_HOST="rschmied@172.17.0.1"
-VIRL_HOST=""
 PROJECTS="projects"
 
 echo "building stuff..."
@@ -11,11 +10,20 @@ echo "building stuff..."
 echo "copying files to destination"
 scp -r ${PROJECTS}/ ${BUILD_HOST}:
 ssh ${BUILD_HOST} "
+
+# activate the environment
 source venv/bin/activate
 cd $PROJECTS
-mkdir LOGS
-touch LOGS/GAGA
-virltester 2>&1 --nocolor --help | tee main.log 
+
+# create the log directory and empty it
+test -d LOGS || mkdir LOGS
+rm LOGS/*
+
+for v in $(ls *.yml); do
+    virltester 2>&1 --nocolor ${v} | tee $(basename -s yml)log
+done
+
+# move all log files into the artifacts dir
 mv *.log LOGS
 "
 
